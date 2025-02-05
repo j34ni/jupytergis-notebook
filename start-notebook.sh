01d3824229a943cd2a7619463fd9d97792e0fa7d
j34ni <jeani@uio.no>
2025-02-05 07:52:43 +0000

    Use modified start-notebook.sh

--- start-notebook.sh
+++ start-notebook.sh
@@ -2,55 +2,61 @@
 
 set -e
 
-# Normalize username if JUPYTERHUB_USER is set
+# Since the name of the volume is escaped wrongly, we have to wrongly escape the
+# usename elsewhere as well.
+# https://github.com/jupyterhub/kubespawner/pull/309
+REAL_JUPYTERHUB_USER=$JUPYTERHUB_USER
 if [ -n "$JUPYTERHUB_USER" ]; then
-  REAL_JUPYTERHUB_USER=$JUPYTERHUB_USER
   JUPYTERHUB_USER=$(python /usr/local/bin/normalize-username.py $JUPYTERHUB_USER)
 fi
 
-# Set HOME directory
 HOME=$(eval echo "$HOME")
-if [ -n "$REAL_JUPYTERHUB_USER" ]; then
-  JUPYTERHUB_USER=$REAL_JUPYTERHUB_USER
-fi
+JUPYTERHUB_USER=$REAL_JUPYTERHUB_USER # Swich back after expanding, as Jupyterhub breaks otherwise.
 
-# Ensure user's home directory exists
 mkdir -p "$HOME"
+#mv /home/notebook /home/oldnotebook
+ln -s "$HOME" /home/notebook
 
-# Create or update symbolic link for notebook
-if [ ! -L "/home/notebook/notebook" ]; then
-    rm -rf "/home/notebook/notebook" 2>/dev/null
-    ln -snf "$HOME" /home/notebook/notebook
+# Exec the specified command or fall back on bash
+if [ $# -eq 0 ]; then
+    cmd=bash
+else
+    cmd=$*
 fi
 
-# Copy .jupyter directory if it doesn't exist
 if [ ! -d "$HOME/.jupyter" ]; then
-    cp -r "/opt/.jupyter" "$HOME/.jupyter"
+	cp -r "/opt/.jupyter" "$HOME/.jupyter"
 fi
 
-# Copy ipcontroller-client.json if it exists
 if [ -f "/tmp/ipcontroller-client.json" ]; then
   mkdir -p "$HOME/.ipython/profile_default/security/"
+  ls -lah "$HOME/.ipython/profile_default/security/"
   cp "/tmp/ipcontroller-client.json" "$HOME/.ipython/profile_default/security/" || true
 fi
 
-# Copy jupyter_notebook_config.py if it doesn't exist
-if [ ! -f "$HOME/.jupyter/jupyter_notebook_config.py" ]; then
-    cp "/opt/.jupyter/jupyter_notebook_config.py" "$HOME/.jupyter"
+if [ ! -f "$HOME/.jupyter/jupyter_server_config.py" ]; then
+	cp -r "/opt/.jupyter/jupyter_server_config.py" "$HOME/.jupyter"
 fi
 
-# Handle shared data directories
+# If we have shared data directories mounted, make the folders available in the users home directory.
 if [ -d "/mnt" ]; then
     for dir in /mnt/*/; do
       if [ -d "$dir" ]; then
-        dirname=${dir##*/}
-        ln -snf "$dir" "$HOME/shared-$dirname"
+        dirname=${dir%*/}     # remove the trailing "/"
+        dirname=${dirname##*/}    # everything after the final "/"
+        if [ -L "$HOME/shared-$dirname" ]; then
+          rm -f "$HOME/shared-$dirname"
+        fi
+
+        ln -sf "/mnt/$dirname" "$HOME/shared-$dirname"
       fi
     done
 fi
 
-# Change to the user's home directory
 cd "$HOME"
 
-# Start JupyterLab
-jupyter-notebook --config "$HOME/.jupyter/jupyter_notebook_config.py"
+if [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
+  jupyter-notebook --config "$HOME/.jupyter/jupyter_notebook_config.py" --SingleUserLabApp.default_url="/lab"
+else
+  jupyter-notebook --config "$HOME/.jupyter/jupyter_notebook_config.py" --SingleUserLabApp.default_url="/tree"
+fi
