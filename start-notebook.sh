2e22c272a65e089acce80a62a19a123d31afd1db
j34ni <jeani@uio.no>
2025-02-28 19:21:12 +0000

    Use hub-singleuser

--- start-notebook.sh
+++ start-notebook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jupyter lab --config "/home/notebook/.jupyter/notebook_config.py"
+jupyterhub-singleuser --config "/home/notebook/.jupyter/notebook_config.py" --SingleUserLabApp.default_url="/lab
