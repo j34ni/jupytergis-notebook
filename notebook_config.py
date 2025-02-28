aebabe3a61b0559d2efaee213fd603883a1ef201
j34ni <jeani@uio.no>
2025-02-28 18:25:46 +0000

    Fixed settings

--- notebook_config.py
+++ notebook_config.py
@@ -1,37 +1,36 @@
-# Configuration file for ipython-notebook.
+import os
 
 CORS_ORIGIN = '*'
 CORS_ORIGIN_HOSTNAME = '*'
 
-if os.environ['CORS_ORIGIN'] != 'none':
+if os.environ.get('CORS_ORIGIN', 'none') != 'none':
     CORS_ORIGIN = os.environ.get('CORS_ORIGIN', '')
-    CORS_ORIGIN_HOSTNAME = CORS_ORIGIN.split('://')[1]
+    CORS_ORIGIN_HOSTNAME = CORS_ORIGIN.split('://')[1] if '://' in CORS_ORIGIN else CORS_ORIGIN
 
 headers = {
     'X-Frame-Options': 'ALLOWALL',
-    'Content-Security-Policy':
-        "; ".join([
-            f"default-src 'self' https: {CORS_ORIGIN}",
-            f"img-src 'self' data: *",
-            f"connect-src 'self' wss://{CORS_ORIGIN_HOSTNAME}",
-            f"style-src 'unsafe-inline' 'self' {CORS_ORIGIN}",
-            f"script-src https: 'unsafe-inline' 'unsafe-eval' 'self' {CORS_ORIGIN}"
-        ])
+    'Content-Security-Policy': "; ".join([
+        f"default-src 'self' https: {CORS_ORIGIN}",
+        "img-src 'self' data: https: blob:",
+        f"connect-src 'self' ws: wss: {CORS_ORIGIN_HOSTNAME}",
+        f"style-src 'self' 'unsafe-inline' {CORS_ORIGIN}",
+        f"script-src 'self' 'unsafe-inline' 'unsafe-eval' https: {CORS_ORIGIN}" 
+    ])
 }
-c = get_config()
 
-# ------------------------------------------------------------------------------
-# NotebookApp configuration
-# ------------------------------------------------------------------------------
+c = get_config()
 
 c.IPKernelApp.pylab = 'inline'
 c.ServerApp.ip = '*'
 c.ServerApp.open_browser = False
 c.ServerApp.quit_button = False
 c.ServerApp.port = 8888
-c.ServerApp.base_url = '/'
+c.ServerApp.base_url = '/' 
 c.ServerApp.trust_xheaders = True
-c.ServerApp.tornado_settings = {'static_url_prefix': '/static/'}
+c.ServerApp.tornado_settings = {
+    'static_url_prefix': '/static/',
+    'headers': headers
+}
 c.ServerApp.root_dir = '/home/notebook'
 c.ServerApp.allow_origin = '*'
 c.ServerApp.log_level = 'DEBUG'
