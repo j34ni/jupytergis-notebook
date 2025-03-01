8503456697cdfb0c40a091cb045b1648525c0657
j34ni <jeani@uio.no>
2025-03-01 13:16:37 +0000

    Fix Deprecation

--- notebook_config.py
+++ notebook_config.py
@@ -1,11 +1,11 @@
 import os
 
 c = get_config()
-c.ServerApp.ip = '0.0.0.0'  # Listen on all interfaces
+c.ServerApp.ip = '0.0.0.0'
 c.ServerApp.open_browser = False
 c.ServerApp.port = 8888
-c.ServerApp.base_url = '/'  # Open OnDemand will proxy this
-c.ServerApp.allow_origin = os.environ.get('JUPYTERHUB_HOST', '*')  # Dynamic from JupyterHub
+c.ServerApp.base_url = '/'
+c.ServerApp.allow_origin = os.environ.get('JUPYTERHUB_HOST', '*')
 c.ServerApp.log_level = 'DEBUG'
-c.ServerApp.root_dir = '/home/jovyan'  # Generic working directory
-c.ServerApp.token = os.environ.get('JUPYTERHUB_API_TOKEN', '')  # Handle JupyterHub token
+c.ServerApp.root_dir = '/home/jovyan'
+c.IdentityProvider.token = os.environ.get('JUPYTERHUB_API_TOKEN', '')
