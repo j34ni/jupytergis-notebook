a897126648af7a10cc1820b8dca1e33d42904275
j34ni <jeani@uio.no>
2025-03-01 13:20:04 +0000

    Deal with JUPYTERHUB_SERVICE_URL missing for standalone execution

--- start-notebook.sh
+++ start-notebook.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# Start JupyterHub single-user server
-exec jupyterhub-singleuser --config=/home/jovyan/.jupyter/notebook_config.py --SingleUserLabApp.default_url=/lab
+# Check if JUPYTERHUB_SERVICE_URL is set
+if [ -z "$JUPYTERHUB_SERVICE_URL" ]; then
+    echo "Running standalone JupyterLab (not under JupyterHub)"
+    exec jupyter-lab --ip=0.0.0.0 --port=8888 --allow-root --NotebookApp.token='' --NotebookApp.base_url=/
+else
+    echo "Running under JupyterHub"
+    exec jupyterhub-singleuser --config=/home/jovyan/.jupyter/notebook_config.py --SingleUserLabApp.default_url=/lab
+fi
