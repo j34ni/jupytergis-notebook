08f63d08eb9bf6c7903e0c7d5389448482854abf
j34ni <jeani@uio.no>
2025-03-01 09:14:41 +0000

    Ensure allow_origin matches the JupyterHub domain and handle authentication

--- notebook_config.py
+++ notebook_config.py
@@ -1,21 +1,17 @@
 import os
 
-CORS_ORIGIN = '*'
-CORS_ORIGIN_HOSTNAME = '*'
-
-if os.environ.get('CORS_ORIGIN', 'none') != 'none':
-    CORS_ORIGIN = os.environ.get('CORS_ORIGIN', '')
-    CORS_ORIGIN_HOSTNAME = CORS_ORIGIN.split('://')[1] if '://' in CORS_ORIGIN else CORS_ORIGIN
+CORS_ORIGIN = os.environ.get('JUPYTERHUB_ACTIVITY_URL', '*')
+CORS_ORIGIN_HOSTNAME = CORS_ORIGIN.split('://')[1] if '://' in CORS_ORIGIN else CORS_ORIGIN
 
 headers = {
     'X-Frame-Options': 'ALLOWALL',
     'Content-Security-Policy': "; ".join([
-        f"default-src 'self' https: {CORS_ORIGIN}",
-        "img-src 'self' data: https: blob: *.tile.openstreetmap.org *.tile.opentopomap.org",
-        f"connect-src 'self' ws: wss: {CORS_ORIGIN_HOSTNAME} https://*.mapbox.com wss://*.jupytergis.org",
-        f"style-src 'self' 'unsafe-inline' {CORS_ORIGIN}",
-        f"script-src 'self' 'unsafe-inline' 'unsafe-eval' https: {CORS_ORIGIN}",
-        "frame-src 'self' https:"
+        f"default-src 'self' 'unsafe-inline' 'unsafe-eval' https: http: data: blob: {CORS_ORIGIN}",
+        f"img-src 'self' data: https: http: blob: *.tile.openstreetmap.org *.tile.opentopomap.org",
+        f"connect-src 'self' ws: wss: https: http: *.mapbox.com *.jupytergis.org {CORS_ORIGIN}",
+        f"style-src 'self' 'unsafe-inline' https: http: {CORS_ORIGIN}",
+        f"script-src 'self' 'unsafe-inline' 'unsafe-eval' https: http: {CORS_ORIGIN}",
+        f"frame-src 'self' https: http: {CORS_ORIGIN}"
     ])
 }
 
@@ -26,14 +22,15 @@ c.ServerApp.ip = '*'
 c.ServerApp.open_browser = False
 c.ServerApp.quit_button = False
 c.ServerApp.port = 8888
-c.ServerApp.base_url = '/' 
+c.ServerApp.base_url = '/'
 c.ServerApp.trust_xheaders = True
 c.ServerApp.tornado_settings = {
     'static_url_prefix': '/static/',
     'headers': headers
 }
 c.ServerApp.root_dir = '/home/notebook'
-c.ServerApp.allow_origin = '*'
+c.ServerApp.allow_origin = CORS_ORIGIN
 c.ServerApp.log_level = 'DEBUG'
 c.ServerApp.allow_remote_access = True
+c.ServerApp.token = os.environ.get('JUPYTERHUB_API_TOKEN', '')
 c.InteractiveShell.ast_node_interactivity = "all"
